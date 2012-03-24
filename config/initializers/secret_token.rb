0e23e13c2238fb828a3fcdb658c60f4a42c13b3f
Rodolfo Spalenza <rodolfo.spalenza@gmail.com>
2012-03-24 01:31:29 -0300

    Projeto inicial com Ruby on Rails e MySQL.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VcomLibary::Application.config.secret_token = '3e218dca0fbe34c827ffc2690a3395ba1e19f4595caac418711dbe3a69c166d12541e46bf0ae4d53190f40577b5baf43989ae727e81d7bdcaf8e9317ce8e4b0e'
